a2e477ac396d0a50698104d147dfa894ff2e3284
Piotr Krawiec <164165@stud.prz.edu.pl>
2023-01-15 11:13:27 +0100

    FINAL

--- Twoj_Eko_Kacik.py
+++ Twoj_Eko_Kacik.py
@@ -1,40 +1,70 @@
 import streamlit as st
 import numpy as np
 import plotly.figure_factory as ff
+import pandas as pd
+import plotly.express as px
 
 
 st.header("Twój Eko Kącik")
 
-slider = st.slider("Czas")
+with st.sidebar:
+    slider = st.slider("Dzień", min_value=1, max_value=500, value=100, step=1)
+
+## Dataset
+d = pd.read_pickle("Notebooks/dataset.pickle")
+cols = ['DE_KN_residential3_circulation_pump', 'DE_KN_residential3_dishwasher',
+       'DE_KN_residential3_freezer',
+       'DE_KN_residential3_refrigerator', 'DE_KN_residential3_washing_machine']
+cols_pred = [
+    'DE_KN_residential3_circulation_pump_pred',
+       'DE_KN_residential3_dishwasher_pred', 'DE_KN_residential3_freezer_pred',
+       'DE_KN_residential3_refrigerator_pred',
+       'DE_KN_residential3_washing_machine_pred'
+        ]
+d_prep = d.drop(columns=['energy_consumption', 'start', 'end', 'step', 'cstep']).resample("D").sum()
+
+def piechart(i, pred):
+    if pred:
+        data = d_prep[cols_pred].iloc[i]
+        fig = px.pie(pd.DataFrame({"names": ['Pompa cyrkulacyjna', 'Zmywarka', 'Zamrażarka', 'Lodówka', 'Pralka'], "values": data.values}), values='values', names='names')
+        return fig
+    else:
+        data = d_prep[cols].iloc[i]
+        fig = px.pie(pd.DataFrame({"names": ['Pompa cyrkulacyjna', 'Zmywarka', 'Zamrażarka', 'Lodówka', 'Pralka'], "values": data.values}), values='values', names='names')
+        return fig
+
+def barplot(i, pred):
+    if pred:
+        data = d_prep[cols_pred].iloc[i]
+        fig = px.bar(pd.DataFrame({"names": ['Pompa cyrkulacyjna', 'Zmywarka', 'Zamrażarka', 'Lodówka', 'Pralka'], "values": data.values}), x='values', y='names', title="Zużycie energi w [kWh]", labels={'values':'Energia w kWh', 'names': "Urządzenie"})
+        return fig
+    else:
+        data = d_prep[cols_pred].iloc[i]
+        fig = px.bar(pd.DataFrame({"names": ['Pompa cyrkulacyjna', 'Zmywarka', 'Zamrażarka', 'Lodówka', 'Pralka'], "values": data.values}), x='values', y='names', title="Zużycie energi w [kWh]", labels={'values':'Energia w kWh', 'names': "Urządzenie"})
+        return fig
 
 col1, col2 = st.columns(2)
 with col1:
-    st.write("Toe będzie kolumna pierwsza")
+    st.write("Dane rzeczywiste")
 
-    x1 = np.random.randn(200) - 2
-    x2 = np.random.randn(200)
-    x3 = np.random.randn(200) + 2
-    hist_data = [x1, x2, x3]
-    group_labels = ['Group 1', 'Group 2', 'Group 3']
-    fig = ff.create_distplot(
-            hist_data, group_labels, bin_size=[.1, .25, .5])
-    st.plotly_chart(fig, use_container_width=True)
+    piechart_true = piechart(slider, False) 
+    barplot_true = barplot(slider, False)
+    st.plotly_chart(piechart_true, use_container_width=True)
+    st.plotly_chart(barplot_true, use_container_width=True)
 
 
 with col2:
-    st.write("To będzie kolumna druga")
-    x1 = np.random.randn(200) - 2
-    x2 = np.random.randn(200)
-    x3 = np.random.randn(200) + 2
-    hist_data = [x1, x2, x3]
-    group_labels = ['Group 1', 'Group 2', 'Group 3']
-    fig = ff.create_distplot(
-            hist_data, group_labels, bin_size=[.1, .25, .5])
-    st.plotly_chart(fig, use_container_width=True)
+    st.write("Dane przewidywane przez AI")
+
+    piechart_pred = piechart(slider, True) 
+    barplot_pred = barplot(slider, True)
+    st.plotly_chart(piechart_pred, use_container_width=True)
+    st.plotly_chart(barplot_pred, use_container_width=True)
 
 
 with st.container():
     metric_col1, metric_col2, metric_col3 = st.columns(3)
-    metric_col1.metric("Cena", 212)
-    metric_col2.metric("Emisja $CO_2$", 1203, delta=21)
-    metric_col3.metric("Top %", slider)
+    metric_col1.metric("Cena", d_prep[cols_pred].iloc[slider].sum() * 1)
+    metric_col2.metric("Emisja $CO_2$ [g]", d_prep[cols_pred].iloc[slider].sum() * 801, 
+                       delta=(d_prep[cols_pred].iloc[slider-1].sum() -d_prep[cols_pred].iloc[slider].sum())*801)
+    metric_col3.metric("Top %", 30)
